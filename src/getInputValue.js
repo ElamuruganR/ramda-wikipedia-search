4a85e9b196961153b830ecc2ab8cb7ad5484a48e
ElamuruganR <elamurugan.r@robosoftin.com>
2021-07-28 15:11:28 +0530

    Fix for 2 calls on pressing Caps letters

--- src/getInputValue.js
+++ src/getInputValue.js
@@ -1,6 +1,13 @@
-import { pathOr, pipe, trim } from 'ramda';
+import { ifElse, map, pathOr, pipe, tap, trim } from 'ramda';
 
 export default pipe(
+    // map(e => e.keyCode===16 ? e.preventDefault() : e),
+    // tap(e => {
+    //             console.log("Value:", e.target.value);
+    //             console.log("Key code:", e.keyCode);
+    //          }
+    // ),
     pathOr('', ['target', 'value']),
+
     trim
 );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ifElse, isEmpty, pipe } from 'ramda';
+import { contains, equals, ifElse, isEmpty, map, pathOr, pipe, reject, tap, complement } from 'ramda';
 import Results from './Results';
 import getInputValue from './getInputValue';
 import getWikipediaSearchUrlFor from './getWikipediaSearchUrlFor';
 
-// const R = require('ramda');
 
 const doNothing = () => {};
+const emptyString = () => "";
 
 const render = markup => {
     const resultsElement = document.getElementById('results');
@@ -24,36 +24,18 @@ const searchAndRenderResults = pipe(
         .catch(err => console.log(err))
     }
 );
+const isShiftKey = pipe(
 
+    pathOr('', ['keyCode']),
+    tap(x => console.log('pathOr keyCode: ',x)),
+    equals(16),
+    tap(x => console.log('retur:',x))
+)
 const makeSearchRequestIfValid = pipe(
-    getInputValue,
-    ifElse(isEmpty, doNothing, searchAndRenderResults)
+    ifElse( isShiftKey,  emptyString, getInputValue),
+    ifElse( isEmpty, doNothing, searchAndRenderResults)
 );
 
 const inputElement = document.querySelector('input');
 
 inputElement.addEventListener('keyup', makeSearchRequestIfValid);
-
-// const Results = ([query, titles, descriptions, links]) => (
-//     `
-//         <h2>Searching for "${query}"</h2>
-//         <ul class="list-group">
-//             ${titles.map(
-//                 (title, index)=>(
-//                     `
-//                     <li class="list-group-item">
-//                         <a href=${links[index]}>${title}</a>
-//                         <p>${descriptions[index]}</p>
-//                     </li>
-//                     `
-//                 )).join('')
-//             }
-//         </ul>
-//     `
-// )
-// const getInputValue = R.pipe(R.pathOr('',['target','value']), R.trim);
-// const baseUrl = 'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=';
-// const getWikipediaSearchUrlFor = R.concat(baseUrl);
-
-
-
